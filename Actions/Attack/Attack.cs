e7ea426e93e2b7a8256333e3a802f0d601401f14
rcorenti <corentin.reyleroux@ynov.com>
2024-11-25 17:32:34 +0100

    add Attack.cs beginning

---
+++ Actions/Attack/Attack.cs
@@ -0,0 +1,26 @@
+﻿using Tp.Actions.Damages;
+using Tp.Characters;
+
+namespace Tp.Actions.Attack;
+
+public class Attack
+{
+    public string Name;
+    public Character Attacker;
+    public Character Target;
+    public int Damage;
+    public Damage TypeDamage;
+
+    public Attack(string name, Character attacker, Character target, int damage, Damage typeDamage)
+    {
+        Name = name;
+        Attacker = attacker;
+        Target = target;
+        Damage = damage;
+        TypeDamage = typeDamage;
+    }
+
+    
+
+    
+}
