d76ae8ac27bdc0d6e687a6424c7cdc5f5c302a89
rcorenti <corentin.reyleroux@ynov.com>
2024-11-25 17:14:42 +0100

    add Warrior.cs beginning

---
+++ Character/Warrior/Warrior.cs
@@ -0,0 +1,22 @@
+﻿using static Tp.Armors.Armors;
+
+namespace Tp.Character.Warrior;
+
+public class Warrior : Character
+{
+    public Warrior(string name,
+            int pv = 100,
+            int pvActual = 100,
+            int physicalAttack = 50,
+            int magicalAttack = 0,
+            decimal dodgeChance = 0.05m,
+            decimal paradeChance = 0.25m,
+            decimal spellChance = 0.1m)
+    : base(name, pv, pvActual, physicalAttack, magicalAttack, Plate, dodgeChance, paradeChance, spellChance)
+    {}
+
+
+   
+    
+    
+}
