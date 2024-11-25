ede5e9f8a0acacabd97483b6e97589b3140873d9
rcorenti <corentin.reyleroux@ynov.com>
2024-11-25 17:15:04 +0100

    add Character.cs beginning

---
+++ Character/Character.cs
@@ -0,0 +1,37 @@
+﻿namespace Tp.Character;
+
+public class Character
+{ 
+        protected string Name { get; set; }
+        protected int Pv { get; set; }
+        protected int PvActual { get; set; }
+        protected int PhysicalAttack { get; set; }
+        protected int MagicalAttack { get; set; }
+        protected Armors.Armors Armor { get; set; }
+        protected decimal DodgeChance { get; set; }
+        protected decimal ParadeChance { get; set; }
+        protected decimal SpellChance { get; set; }
+        
+        
+        protected bool IsAlive { get; set; } =  true;
+
+        public Character(string name, int pv, int pvActual, int physicalAttack, int magicalAttack, Armors.Armors armor, decimal dodgeChance, decimal paradeChance, decimal spellChance)
+        {
+                Name = name;
+                Pv = pv;
+                PvActual = pvActual;
+                PhysicalAttack = physicalAttack;
+                MagicalAttack = magicalAttack;
+                Armor = armor;
+                DodgeChance = dodgeChance;
+                ParadeChance = paradeChance;
+                SpellChance = spellChance;
+        }
+        
+        
+
+     
+    
+    
+    
+}
