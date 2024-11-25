6d686e676bc58f6afc7ae496a697fd10a41369fd
rcorenti <corentin.reyleroux@ynov.com>
2024-11-25 17:16:08 +0100

    add Thief.cs beginning

---
+++ Character/Thief/Thief.cs
@@ -0,0 +1,15 @@
+﻿namespace Tp.Character.Thief;
+
+public class Thief : Character
+{
+    public Thief(string name,
+        int pv = 80,
+        int pvActual = 80,
+        int physicalAttack = 55,
+        int magicalAttack = 0,
+        decimal dodgeChance = 0.15m,
+        decimal paradeChance = 0.25m,
+        decimal spellChance = 0.25m)
+        : base(name, pv, pvActual, physicalAttack, magicalAttack, Armors.Armors.Leather, dodgeChance, paradeChance, spellChance)
+    {}
+}
