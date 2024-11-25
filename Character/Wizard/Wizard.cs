3facaec47673730ecc241773a554a0ad30871875
rcorenti <corentin.reyleroux@ynov.com>
2024-11-25 17:14:14 +0100

    add Wizard.cs beginning

---
+++ Character/Wizard/Wizard.cs
@@ -0,0 +1,16 @@
+﻿namespace Tp.Character.wizard;
+
+public class Wizard: Character
+{
+    public Wizard(string name,
+        int pv = 60,
+        int pvActual = 60,
+        int physicalAttack = 0,
+        int magicalAttack = 75,
+        decimal dodgeChance = 0.05m,
+        decimal paradeChance = 0.05m,
+        decimal spellChance = 0.25m)
+        : base(name, pv, pvActual, physicalAttack, magicalAttack, Armors.Armors.Fabric, dodgeChance, paradeChance, spellChance)
+    {}
+    
+}
