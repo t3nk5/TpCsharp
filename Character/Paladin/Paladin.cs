35fbb3ec0c829bf1c43abe0ece3f3872e132eebc
rcorenti <corentin.reyleroux@ynov.com>
2024-11-25 17:15:47 +0100

    add Paladin.cs beginning

---
+++ Character/Paladin/Paladin.cs
@@ -0,0 +1,15 @@
+﻿namespace Tp.Character.Paladin;
+
+public class Paladin : Character
+{
+    public Paladin(string name,
+        int pv = 95,
+        int pvActual = 95,
+        int physicalAttack = 40,
+        int magicalAttack = 40,
+        decimal dodgeChance = 0.05m,
+        decimal paradeChance = 0.1m,
+        decimal spellChance = 0.20m)
+        : base(name, pv, pvActual, physicalAttack, magicalAttack, Armors.Armors.Mesh, dodgeChance, paradeChance, spellChance)
+    {}
+}
