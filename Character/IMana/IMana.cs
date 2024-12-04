4ff58f9ba12a1d7d19936692f08fe57a1400509d
rcorenti <corentin.reyleroux@ynov.com>
2024-12-04 20:26:12 +0100

    add IMana.cs

---
+++ Character/IMana/IMana.cs
@@ -0,0 +1,13 @@
+﻿namespace Tp.Character.IMana
+{
+
+    public interface IMana
+    {
+        int Mana { get; set; }
+        int MaxMana { get; set; }
+        
+        void DrinkMana();
+        void UseMana(int mana);
+
+    }
+}
