bea9eaff2e2db76edcb5226c0ac0ec037ab7b8c0
rcorenti <corentin.reyleroux@ynov.com>
2024-12-05 19:00:41 +0100

    add IMana in Wizard.cs

--- Character/Wizard/Wizard.cs
+++ Character/Wizard/Wizard.cs
@@ -6,8 +6,9 @@
     using Spell.TargetType;
     
     
-    public class Wizard : Character
+    public class Wizard : Character, IMana.IMana
     {
+        public int MaxMana { get; set; }
 
         public Wizard(string name)
         {
@@ -21,15 +22,48 @@
             ParadeChance = 0.05;
             SpellResistanceChance = 0.25;
             Speed = 75;
+            
             Mana = 100;
+            MaxMana = 100;
             
-            Spells.Add(new Spell("Frost Bolt", 1, Target.EnemyTarget, 15));
-            Spells.Add(new Spell("Frost Barrier ", 2, Target.Yourself, 25));
-            Spells.Add(new Spell("Blizzard", 2, Target.TeamEnemiesTarget, 25));
             
+            /*
+            Spells.Add(new Spell("Frost Bolt", 2, Target.EnemyTarget, 15));
+            Spells.Add(new Spell("Frost Barrier ", 3, Target.Yourself, 25));
+            Spells.Add(new Spell("Blizzard", 3, Target.TeamEnemiesTarget, 25));
+            */
         }
-        
-        
 
+        public void DrinkMana()
+        {
+            if (Mana + MaxMana / 2 >= MaxMana)
+            {
+                Mana = MaxMana;
+            }
+            else
+            {
+                Mana += MaxMana/2;
+            }
+            Console.WriteLine($"{Name} drink mana and gain {MaxMana/2} mana. \n{Name} have : {Mana}/{MaxMana} mana.");
+        }
+
+        public void UseMana(int mana)
+        {
+            if (Mana - mana <= 0)
+            {
+                Console.WriteLine($"Your turn is blown because {Name} doesn't have enough mana.");
+            }
+            else
+            {
+                Mana -= mana;
+                Console.WriteLine($"{Name} use mana {mana}. He has now {Mana}/{MaxMana} mana."); 
+            }
+        }
+
+
+        public override void Choice(string choice)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
