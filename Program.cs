8146aff49fe1c46f5006b195a20b1022f028c951
rcorenti <corentin.reyleroux@ynov.com>
2024-12-05 19:02:49 +0100

    start Game

--- Program.cs
+++ Program.cs
@@ -1,30 +1,13 @@
-﻿using Tp.Actions.Attack;
-using Tp.Actions.Damages;
-using Tp.Characters;
-using Tp.Characters.Warrior;
+﻿using Tp.Character.Warrior;
+
+namespace Tp;
 
 class Program
 {
     static void Main()
     {
-        var steve = new Warrior("Steve");
-        var thanos = new Warrior("Thanos");
+        var game = new Game.Game();
+        game.BeginGame();
         
-        var teamTarget = new List<Character>{thanos};
-        
-        steve.Attack(teamTarget, "Heroic Strike");
-        Console.WriteLine(steve.ToString());
-
-        /*
-
-        Console.WriteLine("Beginning Battle of Warrior!");
-        steve.Attack(thanos);
-        thanos.BattleCry();
-        thanos.Attack(steve);
-
-        Console.WriteLine($"\nFinales statistics: ");
-        Console.WriteLine($"{steve.Name} : {steve.PvActual}/{steve.Pv} PV.");
-        Console.WriteLine($"{thanos.Name} : {thanos.PvActual}/{thanos.Pv} PV.");
-        */
     }
 }
