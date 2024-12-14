0b3ef62a626ce493fa7335ed5663ecc1f2782231
rcorenti <corentin.reyleroux@ynov.com>
2024-12-14 18:44:56 +0100

    Update files settings

--- obj/Release/net8.0/Tp.AssemblyInfo.cs
+++ obj/Release/net8.0/Tp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b9c8bc16896f8e8198f37c122747475ed213308")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tp")]
