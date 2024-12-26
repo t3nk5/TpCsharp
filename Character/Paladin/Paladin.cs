e72dcdceb7e528ba4c0234bc807f57e5a7578ef7
rcorenti <corentin.reyleroux@ynov.com>
2024-12-26 16:39:16 +0100

    add commentary Paladin.cs

--- Character/Paladin/Paladin.cs
+++ Character/Paladin/Paladin.cs
@@ -6,11 +6,17 @@
     using Spell.TargetType;
     using Spell.DamageType;
 
-
+    /// <summary>
+    /// Represents a Paladin character, a type of warrior with both physical and magical abilities.
+    /// </summary>
     public class Paladin : Character
     {
+        /// <summary>
+        /// Constructor for initializing the Paladin character with default values.
+        /// </summary>
         public Paladin()
         {
+            //Set every value of Paladin
             Name = "Paladin";
             PvActual = 95;
             Pv = 95;
@@ -23,8 +29,8 @@
             
             Speed = 40; 
             Mana = 60;
+            
             Spells.Add(new Spell("Cry Of Battle", 3, 25, this, Target.Team, Damage.Other, 0, null));
-
             Spells.Add(new Spell("RIP my ACL", 2, PhysicalAttack, this, Target.EnemyTarget, Damage.Physical, 5, null));
             Spells.Add(new Spell("Judgment", 2, MagicalAttack, this, Target.EnemyTarget, Damage.Magical, 10, null));
             Spells.Add(new Spell("Flash of Light", 2, (int)1.25 * MagicalAttack, this, Target.Partner, Damage.Heal, 25, null));
